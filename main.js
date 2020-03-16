7d4db7f712b5945edda4fbd119410f806262e3a8
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-16 19:39:15 +0100

    done

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
-const endTime = new Date('2022-01-25 22:34:00').getTime();
+const endTime = new Date('2020-03-25 22:34:00').getTime();
+
+const spanD = document.querySelector('span.d');
+const spanH = document.querySelector('span.h');
+const spanM = document.querySelector('span.m');
+const spanS = document.querySelector('span.s');
+
 
 setInterval(() => {
     const nowTime = new Date().getTime();
-    const time = Math.floor((endTime - nowTime) / 1000);
+    // const time = Math.floor((endTime - nowTime) / 1000);
 
-    console.log(time);
+    const time = endTime - nowTime;
+
+    let days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+
+    days = days < 10 ? `0${days}` : days;
+
+    console.log(days);
+
+    let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+
+    hours = hours < 10 ? `0${hours}` : hours;
+
+    let minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    let secs = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+
+    secs = secs < 10 ? `0${secs}` : secs;
+
+    spanD.textContent = days;
+    spanH.textContent = hours;
+    spanM.textContent = minutes;
+    spanS.textContent = secs;
+
 }, 1000)
