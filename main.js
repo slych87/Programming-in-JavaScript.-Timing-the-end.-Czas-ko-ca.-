e9f5a62c30dae8b9a1d6ad1b3eeeb64c3d8a6f77
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-16 13:26:03 +0100

    we remove decimal values

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const endTime = new Date('2022-01-25 22:34:00').getTime();
 
 setInterval(() => {
     const nowTime = new Date().getTime();
-    const time = (endTime - nowTime) / 1000;
+    const time = Math.floor((endTime - nowTime) / 1000);
 
     console.log(time);
 }, 1000)
