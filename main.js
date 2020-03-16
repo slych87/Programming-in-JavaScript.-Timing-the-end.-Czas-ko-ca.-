9b793c5711029980949baba997b1a2a355aedf9b
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-16 13:22:38 +0100

    delete millisecond

---
+++ main.js
@@ -0,0 +1,8 @@
+const endTime = new Date('2022-01-25 22:34:00').getTime();
+
+setInterval(() => {
+    const nowTime = new Date().getTime();
+    const time = (endTime - nowTime) / 1000;
+
+    console.log(time);
+}, 1000)
